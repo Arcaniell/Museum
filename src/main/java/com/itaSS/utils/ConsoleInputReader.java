819036766997182b2afaf264c95a0f17d6dd17df
Yura Sterniuk <sterniuk.y@gmail.com>
2014-12-10 10:37:41 +0200

    Add methods for reading Date, selecting Enum from System.in

--- src/main/java/com/itaSS/utils/ConsoleInputReader.java
+++ src/main/java/com/itaSS/utils/ConsoleInputReader.java
@@ -3,6 +3,11 @@ package com.itaSS.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 public final class ConsoleInputReader {
     private static BufferedReader inConsole = new BufferedReader(new InputStreamReader(System.in));
@@ -20,4 +25,42 @@ public final class ConsoleInputReader {
     public static void close() throws IOException {
         inConsole.close();
     }
+
+    public static Date readDate() {
+        Date result;
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        try {
+            String input = readLine();
+            if (!input.equals("")) {
+                result = new Date(dateFormat.parse(input).getTime());
+                return result;
+            }
+        } catch (ParseException e) {
+            System.out.println("Parse err");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Enum<?> selectEnum(Class<? extends Enum<?>> type) {
+        int counter = 1;
+        for (Enum<?> val : type.getEnumConstants()) {
+            System.out.print("#" + (counter++) + " " +val.toString() + " \n");
+        }
+        System.out.println();
+        Enum<?> material;
+        String input = readLine();
+        if (!input.equals("") && checkMenuSelect(input)) {
+            int selected = Integer.valueOf(input) - 1;
+            material = Arrays.asList(type.getEnumConstants()).get(selected);
+            return material;
+        }
+        return null;
+    }
+
+    private static boolean checkMenuSelect(String string) {
+        return string.matches("[1-9][0-9]*");
+    }
+
 }
